40c9bde0178163d2083aeb68e1315f698b2e50fd
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-07-28 12:52:52 +0000

    Added code to retrieve related provider
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/templatedisplay/trunk@10776 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_templatedisplay_tceforms.php
+++ class.tx_templatedisplay_tceforms.php
@@ -24,6 +24,8 @@
 * $Id: class.tx_datadisplay_pi1.php 3938 2008-06-04 08:39:01Z fsuter $
 ***************************************************************/
 
+require_once(t3lib_extMgm::extPath('basecontroller', 'class.tx_basecontroller_div.php'));
+
 /**
  * TCEform custom field for template mapping
  *
@@ -48,18 +50,58 @@ class tx_templatedisplay_tceforms {
 		$formField .= '<input type="hidden" name="'.$PA['itemFormElName'].'" value="Hello" />';
 //		$formField .= t3lib_div::view_array($PA);
 //		$formField .= t3lib_div::view_array($GLOBALS['TYPO3_CONF_VARS']);
-		$formField .= $this->getMMTableName();
+		try {
+			$provider = $this->getRelatedProvider($PA['row']);
+		}
+		catch (Exception $e) {
+			$formField .= tx_basecontroller_div::wrapMessage($e->getMessage());
+        }
 		return $formField;
 	}
 
 	/**
      * This method returns the name of the table where the relations between
      * Data Providers and Controllers are saved
+     * (this has been abstracted in a method in case the was of retrieving this table mame is changed in the future
+     * e.g. by defining a bidirection MM-relation to the display controller, in which case the name
+     * would be retrieved from the TCA instead)
      *
      * @return	string	Name of the table
      */
 	protected function getMMTableName() {
 		return $GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$this->extKey]['mm_table'];
+    }
+
+	/**
+     * This method retrieves the controller which calls this specific instance of template display
+     *
+     * @param	array	$row: database record corresponding the instance of template display
+     */
+	protected function getRelatedProvider($row) {
+		// Get the tt_content record(s) the template display instance is related to
+		$mmTable = $this->getMMTableName();
+		$rows = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('uid_local', $mmTable, "uid_foreign = '".$row['uid']."'");
+		$numRows = count($rows);
+
+		// The template display instance is not related yet
+		if ($numRows == 0) {
+			throw new Exception('No controller found');
+        }
+
+		// The template display instance is related to exactly one tt_content record (easy case)
+		elseif ($numRows == 1) {
+			$tt_contentRecord = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('CType', 'tt_content', "uid = '".$rows[0]['uid_local']."'");
+			$controller = t3lib_div::makeInstanceService('datacontroller', $tt_contentRecord[0]['CType']);
+			$controller->loadControllerData($rows[0]['uid_local']);
+			$provider = $controller->getPrimaryProvider();
+			return $provider;
+       }
+
+		// The template display instance is related to more than one tt_content records
+		// Some additional checks must be performed
+		else {
+			throw new Exception('More than one controller found');
+        }
     }
 }
 
