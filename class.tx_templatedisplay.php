3e9b38981793a49b2c8a291db5532d3b2e445ba7
omic <omic@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-08-17 21:38:27 +0000

    backup current development
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/templatedisplay/trunk@11178 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_templatedisplay.php
+++ class.tx_templatedisplay.php
@@ -3,6 +3,7 @@
 *  Copyright notice
 *
 *  (c) 2007 Francois Suter (Cobweb) <typo3@cobweb.ch>
+*  (c) 2008 Fabien Udriot (Ecodev) <fabien.udriot@ecodev.ch>
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -29,9 +30,10 @@
 require_once(t3lib_extMgm::extPath('basecontroller', 'services/class.tx_basecontroller_consumerbase.php'));
 
 /**
- * Plugin 'Data Displayer' for the 'datadisplay' extension.
+ * Plugin 'Data Displayer' for the 'templatedisplay' extension.
  *
  * @author	Francois Suter (Cobweb) <typo3@cobweb.ch>
+ * @author	Fabien Udriot <fabien.udriot@ecodev.ch>
  * @package	TYPO3
  * @subpackage	tx_templatedisplay
  */
@@ -46,79 +48,6 @@ class tx_templatedisplay extends tx_basecontroller_consumerbase {
 	protected static $currentIndex = 0;
 
 	/**
-	 * This method can be called when displaying a nested table inside the data structure
-	 * It avoids the overhead of the full initialisation
-	 *
-	 * @return	string	The HTML content to display
-	 */
-	function getSubResult() {
-//t3lib_div::debug($this->conf);
-		$subContent = '';
-		$subConfig = $this->getConfigForTable($this->conf['name']);
-//t3lib_div::debug($subConfig);
-		$limit = (isset($subConfig['limit'])) ? $subConfig['limit'] : 0;
-			// Look for the correct subtable
-		if (isset(self::$structure['records'][self::$currentIndex]['sds:subtables'])) {
-			foreach (self::$structure['records'][self::$currentIndex]['sds:subtables'] as $subtableData) {
-				if ($subtableData['name'] == $this->conf['name']) {
-					$theSubtable = $subtableData['records'];
-					break;
-				}
-			}
-		}
-			// Instantiate content object
-		$localCObj = t3lib_div::makeInstance('tslib_cObj');
-			// Render the subtable data, if defined
-			#print_r($theSubtable);
-		if (isset($theSubtable)) {
-//t3lib_div::debug($theSubtable);
-			$counter = 0;
-			$hasFieldConfig = (isset($subConfig['field.'])) ? true : false;
-			foreach ($theSubtable as $record) {
-				$localCObj->start($record);
-				$subrowContent = '';
-
-// If there's a generic configuration for the fields, loop on all fields and apply configuration to each
-
-				if ($hasFieldConfig) {
-					foreach ($record as $field) {
-						if ($field == 'section_count') continue; // Ignore special section count field
-						$subrowContent .= $localCObj->stdWrap($field, $subConfig['field.']);
-					}
-				}
-				$subContent .= $localCObj->stdWrap($subrowContent, $subConfig['row.']);
-				$counter++;
-				if ($limit > 0 && $counter >= $limit) break;
-			}
-		}
-
-// Apply global stdWrap
-
-		$subContent = $localCObj->stdWrap($subContent, $subConfig['allWrap.']);
-		return $subContent;
-	}
-
-	/**
-	 * This method checks whether a config exists for a given table name
-	 * If yes, it returns that config, if not it returns the default one
-	 *
-	 * @param	string	table name
-	 *
-	 * @return	array	TS configuration for the rendering of the table
-	 */
-	function getConfigForTable($tableName) {
-		if (isset($this->conf['configs.'][$tableName.'.'])) {
-			return $this->conf['configs.'][$tableName.'.'];
-		}
-		elseif (isset($this->conf['configs.']['default.'])) {
-			return $this->conf['configs.']['default.'];
-		}
-		else { // Default configuration shouldn't be missing really. Bad boy! TODO: issue error message
-			return array();
-		}
-	}
-
-	/**
 	 * This method is used to pass a TypoScript configuration (in array form) to the Data Consumer
 	 *
 	 * @param	array	$conf: TypoScript configuration for the extension
@@ -127,7 +56,7 @@ class tx_templatedisplay extends tx_basecontroller_consumerbase {
 		$this->conf = $conf;
 	}
 
-// Data Consumer interface methods
+	// Data Consumer interface methods
 
 	/**
 	 * This method returns the type of data structure that the Data Consumer can use
@@ -177,118 +106,61 @@ class tx_templatedisplay extends tx_basecontroller_consumerbase {
 	 */
 	public function startProcess() {
 		
-		/* CHANGE THIS
-		 * Transform the string from field mappings into an array.
-		 * This array can be considered as a datasource containing the mapping information btw a marker and a field.
-		 */
-		$datasource = json_decode($row['mappings'],true);	
-					
-					
-			// Get record where the details of the data display are stored
-		$tableTCA = $GLOBALS['TCA'][$this->table];
+		// Fetches mappings + template file
 		$whereClause = "uid = '".$this->uid."'";
 		$whereClause .= $GLOBALS['TSFE']->sys_page->enableFields($this->table, $GLOBALS['TSFE']->showHiddenRecords);
-		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery('*', $this->table, $whereClause);
-		if ($GLOBALS['TYPO3_DB']->sql_num_rows($res) == 0) {
+
+		$record = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('mappings,template',$this->table,$whereClause);
+		if(!isset($record[0]['mappings'])){
+			$this->result .= '<div style="color :red; font-weight: bold">No templatedisplay has been found for uid = '.$this->uid . '.</div>';
+			$this->result .= '<div style="color :red; font-weight: bold; margin-top: 10px;">Templatedisplay\'s record may be deleted or hidden.</div>';
 			return false;
 		}
-		else {
-			$this->result = '';
 
-// Get the name and configuration for the main table
-
-			$maintableConf = $this->getConfigForTable(self::$structure['name']);
-
-// Set general display flag
-// May be set to false on some conditions
-
-			$display = true;
-
-// Continue only if display is true
-
-			if ($display) {
-
-// Display the data
-// First set some flags depending on TS template
-
-				$hasFieldConfig = (isset($maintableConf['field.'])) ? true : false;
-				$sectionBreak = (empty($maintableConf['section'])) ? '' : $maintableConf['section.']['field'];
-
-// Initialise some values
-// Get an instance of tslib_cobj for rendering the data
-
-				$localContent = '';
-				$sectionContent = '';
-				$oldSectionValue = '';
-				$sectionCount = 1;
-				$localCObj = t3lib_div::makeInstance('tslib_cObj');
-
-				foreach (self::$structure['records'] as $index => $record) {
-					$record['section_count'] = $sectionCount;
-					self::$currentIndex = $index;
-
-// Load the local cObj with data from the database
-
-					$localCObj->start($record);
-
-// If sections are activated, check if a new section has started
-
-					if (!empty($sectionBreak)) {
-						$newSectionValue = $record[$sectionBreak];
-
-// New section
-
-						if ($newSectionValue != $oldSectionValue) {
-
-// Wrap content from previous section
-// Then add section header
-
-							if ($sectionCount > 1) $localContent .= $localCObj->stdWrap($sectionContent, $maintableConf['section.']['content.']);
-							$localContent .= $localCObj->stdWrap($newSectionValue, $maintableConf['section.']['header.']);
-
-// Switch section value, reinitialise section content and increase section count
-
-							$oldSectionValue = $newSectionValue;
-							$sectionContent = '';
-							$sectionCount++;
-						}
-					}
-					$rowContent = '';
-
-// If there's a generic configuration for the fields, loop on all fields and apply configuration to each
-
-					if ($hasFieldConfig) {
-						foreach ($record as $field) {
-							if ($field == 'section_count') continue; // Ignore special section count field
-							$rowContent .= $localCObj->stdWrap($field, $maintableConf['field.']);
-						}
-					}
-
-// Apply stdWrap to the row (i.e. data record)
-
-					$sectionContent .= $localCObj->stdWrap($rowContent, $maintableConf['row.']);
-				}
-
-// If sections were not activated, just take the result from the loop
-// Otherwise apply section content stdWrap to last section
-
-				if (empty($sectionBreak)) {
-					$localContent = $sectionContent;
-				}
-				else {
-					$localContent .= $localCObj->stdWrap($sectionContent, $maintableConf['section.']['content.']);
-				}
-
-// Apply global stdWrap
-
-				$content .= $localCObj->stdWrap($localContent, $maintableConf['allWrap.']);
-			}
-			else {
-				$content = '';
-			}
-
-			$this->result .= $content;
+		// Loads the template file
+		$templatePath = 'uploads/tx_templatedisplay/'.$record[0]['template'];
+		if(is_file($templatePath)){
+			$templateCode = file_get_contents($templatePath);
 		}
+		
+		// Transforms the string from field mappings into a PHP array.
+		// This array contains the mapping information btw a marker and a field.
+		$datasources = json_decode($record[0]['mappings'],true);
+		
+		#$string = '<!-- ###LOOP.pages### begin -->';
+		#$pattern = '/LOOP\.([^#]+)### begin/i';
+		#preg_match_all($pattern,$templateCode,$matches);
+		
+		$marker = '###LOOP.' . self::$structure['name'] . '###';
+		$subTemplate = t3lib_parsehtml::getSubpart($templateCode, $marker);
+//		print_r(self::$structure['header']);
+
+		$langMarkerArray = array();
+		foreach (self::$structure['header'] as $index => $labelArray) {
+			$langMarkerArray['###LABEL.' . $index . '###'] = $labelArray['label'];
+		}
+
+		$subTemplateContent = '';
+		foreach (self::$structure['records'] as $records) {
+			$_markerArray = array();
+			if (is_array($records)) {
+				foreach ($records as $index => $record) {
+					if ($index != 'sds:subtables') {
+						$_markerArray['###FIELD.'.$index.'###'] = $record;
+					}
+				}
+				// Merge field content with label translation
+				$_markerArray = array_merge($_markerArray, $langMarkerArray);
+				$subTemplateContent .= t3lib_parsehtml::substituteMarkerArray($subTemplate, $_markerArray);
+			}
+		}
+
+		// Search / replace label translation
+		$templateCode = t3lib_parsehtml::substituteMarkerArray($templateCode, $langMarkerArray);
+
+		// Substitute subpart
+		$templateContent = t3lib_parsehtml::substituteSubpart($templateCode, $marker, $subTemplateContent);
+		$this->result = $templateContent;
 	}
 
 	/**
@@ -303,8 +175,8 @@ class tx_templatedisplay extends tx_basecontroller_consumerbase {
 
 
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/datadisplay/pi1/class.tx_templatedisplay_pi1.php'])	{
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/datadisplay/pi1/class.tx_templatedisplay_pi1.php']);
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/templatedisplay/pi1/class.tx_templatedisplay_pi1.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/templatedisplay/pi1/class.tx_templatedisplay_pi1.php']);
 }
 
 ?>
