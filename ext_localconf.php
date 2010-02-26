18ed81c28c9e87b31726ff08593bcf098a3b4d67
fabien_u <fabien_u@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-02-26 18:10:03 +0000

    First commit
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/templatedisplay/trunk@30525 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_localconf.php
@@ -0,0 +1,43 @@
+<?php
+if (!defined ('TYPO3_MODE')) {
+ 	die ('Access denied.');
+}
+t3lib_extMgm::addUserTSConfig('
+	options.saveDocNew.tx_templatedisplay_displays=1
+');
+
+// Register method with generic BE ajax calls handler
+// (as from TYPO3 4.2)
+
+$TYPO3_CONF_VARS['BE']['AJAX']['templatedisplay::saveConfiguration'] = 'typo3conf/ext/templatedisplay/class.tx_temlatedisplay_ajax.php:tx_templatedisplay_ajax->saveConfiguration';
+$TYPO3_CONF_VARS['BE']['AJAX']['templatedisplay::saveTemplate'] = 'typo3conf/ext/templatedisplay/class.tx_temlatedisplay_ajax.php:tx_templatedisplay_ajax->saveTemplate';
+/*
+ * Hook for loading Javascript and CSS in the backend
+ */
+if (TYPO3_MODE == 'BE')	{
+	require_once(t3lib_extMgm::extPath('templatedisplay').'hook/user_addBackendLibrary.php');
+}
+$TYPO3_CONF_VARS['SC_OPTIONS']['typo3/template.php']['preStartPageHook'][] = 'user_addBackendLibrary';
+
+// Register as Data Consumer service
+// Note that the subtype corresponds to the name of the database table
+t3lib_extMgm::addService($_EXTKEY,  'dataconsumer' /* sv type */,  'tx_templatedisplay_dataconsumer' /* sv key */,
+	array(
+
+		'title' => 'Data Display Engine',
+		'description' => 'Generic Data Consumer for recordset-type data structures',
+
+		'subtype' => 'tx_templatedisplay_displays',
+
+		'available' => TRUE,
+		'priority' => 50,
+		'quality' => 50,
+
+		'os' => '',
+		'exec' => '',
+
+		'classFile' => t3lib_extMgm::extPath($_EXTKEY, 'class.tx_templatedisplay.php'),
+		'className' => 'tx_templatedisplay',
+	)
+);
+?>
---
+++ ext_tables.php
@@ -0,0 +1,58 @@
+<?php
+if (!defined ('TYPO3_MODE')) {
+	die ('Access denied.');
+}
+
+t3lib_extMgm::allowTableOnStandardPages('tx_templatedisplay_displays');
+
+	// Include class for custom TCEforms field
+require_once(t3lib_extMgm::extPath('templatedisplay', 'class.tx_templatedisplay_tceforms.php'));
+
+	// TCA ctrl for new table
+$TCA['tx_templatedisplay_displays'] = array(
+	'ctrl' => array(
+		'title'     => 'LLL:EXT:templatedisplay/locallang_db.xml:tx_templatedisplay_displays',		
+		'label'     => 'title',
+		'tstamp'    => 'tstamp',
+		'crdate'    => 'crdate',
+		'cruser_id' => 'cruser_id',
+		'default_sortby' => 'ORDER BY title',
+		'delete' => 'deleted',	
+		'enablecolumns' => array(
+			'disabled' => 'hidden',
+		),
+		'dynamicConfigFile' => t3lib_extMgm::extPath($_EXTKEY).'tca.php',
+		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY).'icon_tx_templatedisplay_displays.png',
+	),
+);
+
+
+
+	// Add a wizard for adding a datadisplay
+$addTemplateDisplayWizard = array(
+						'type' => 'script',
+						'title' => 'LLL:EXT:templatedisplay/locallang_db.xml:wizards.add_templatedisplay',
+						'script' => 'wizard_add.php',
+						'icon' => 'EXT:templatedisplay/wizard_icon.gif',
+						'params' => array(
+								'table' => 'tx_templatedisplay_displays',
+								'pid' => '###CURRENT_PID###',
+								'setValue' => 'set'
+							)
+						);
+$TCA['tt_content']['columns']['tx_displaycontroller_consumer']['config']['wizards']['add_templatedisplay'] = $addTemplateDisplayWizard;
+
+
+
+
+
+	// Register templatedisplay with the Display Controller as a Data Consumer
+t3lib_div::loadTCA('tt_content');
+$TCA['tt_content']['columns']['tx_displaycontroller_consumer']['config']['allowed'] .= ',tx_templatedisplay_displays';
+
+	// Define table name that contains the MM relationships
+$TYPO3_CONF_VARS['EXTCONF'][$_EXTKEY]['mm_table'] = 'tx_displaycontroller_consumers_mm';
+
+	// Define the path to the static TS files
+t3lib_extMgm::addStaticFile($_EXTKEY, 'static/', 'Template Display');
+?>
