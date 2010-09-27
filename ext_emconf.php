66a7676886cc71ab41318257cc9f9dfed1e96667
fabien_u <fabien_u@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-09-27 13:13:05 +0000

    templatedisplay: released on TER
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/templatedisplay/trunk@38651 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,27 +1,27 @@
 <?php
 
 ########################################################################
-# Extension Manager/Repository config file for ext: "templatedisplay"
+# Extension Manager/Repository config file for ext "templatedisplay".
 #
-# Auto generated 23-07-2008 13:29
+# Auto generated 27-09-2010 15:12
 #
 # Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'Template-based Data Display (Data Consumer)',
-	'description' => 'Use HTML-based templates to display any kind of data returned by a Data Provider, thanks a to user-friendly mapping interface.',
+	'title' => 'HTML-based Data Consumer - Tesseract project',
+	'description' => 'Use HTML-based templates to display any kind of data returned by a Data Provider, thanks a to user-friendly mapping interface. More info on http://www.typo3-tesseract.com',
 	'category' => 'fe',
 	'author' => 'Francois Suter (Cobweb) / Fabien Udriot',
 	'author_email' => 'typo3@cobweb.ch',
 	'shy' => '',
-	'dependencies' => '',
+	'dependencies' => 'tesseract',
 	'conflicts' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'stable',
 	'internal' => '',
 	'uploadfolder' => 0,
 	'createDirs' => '',
@@ -29,18 +29,20 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.3.6',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.3.0-0.0.0',
-			'tesseract' => '0.7.0-0.0.0',
+			'tesseract' => '0.2.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"8077";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"d43a";s:14:"ext_tables.php";s:4:"4be3";s:14:"ext_tables.sql";s:4:"14fc";s:36:"icon_tx_templatedisplay_displays.gif";s:4:"475a";s:16:"locallang_db.xml";s:4:"b5c6";s:7:"tca.php";s:4:"ebc7";s:19:"doc/wizard_form.dat";s:4:"b285";s:20:"doc/wizard_form.html";s:4:"25bc";}',
+	'_md5_values_when_last_written' => 'a:56:{s:9:"ChangeLog";s:4:"1ab9";s:10:"README.txt";s:4:"db38";s:20:"class.ext_update.php";s:4:"9931";s:32:"class.tx_temlatedisplay_ajax.php";s:4:"3162";s:28:"class.tx_templatedisplay.php";s:4:"72e7";s:37:"class.tx_templatedisplay_tceforms.php";s:4:"6126";s:16:"ext_autoload.php";s:4:"c543";s:21:"ext_conf_template.txt";s:4:"f2bd";s:12:"ext_icon.gif";s:4:"e0c8";s:12:"ext_icon.png";s:4:"aaa6";s:17:"ext_localconf.php";s:4:"9767";s:14:"ext_tables.php";s:4:"f2b4";s:14:"ext_tables.sql";s:4:"801d";s:36:"icon_tx_templatedisplay_displays.gif";s:4:"475a";s:36:"icon_tx_templatedisplay_displays.png";s:4:"3dbb";s:7:"tca.php";s:4:"505e";s:15:"wizard_icon.gif";s:4:"3075";s:43:"Resources/Private/Language/locallang_db.xml";s:4:"2782";s:39:"Resources/Private/Snippets/snippets.xml";s:4:"a5c2";s:48:"Resources/Private/Templates/templatedisplay.html";s:4:"a960";s:34:"Resources/Public/images/accept.png";s:4:"036a";s:33:"Resources/Public/images/email.png";s:4:"e7f6";s:33:"Resources/Public/images/empty.png";s:4:"81a2";s:33:"Resources/Public/images/error.png";s:4:"c847";s:39:"Resources/Public/images/exclamation.png";s:4:"1ee9";s:33:"Resources/Public/images/image.png";s:4:"c2fe";s:41:"Resources/Public/images/imageResource.png";s:4:"d928";s:32:"Resources/Public/images/link.png";s:4:"49be";s:40:"Resources/Public/images/linkToDetail.png";s:4:"49be";s:38:"Resources/Public/images/linkToFile.png";s:4:"49be";s:38:"Resources/Public/images/linkToPage.png";s:4:"49be";s:35:"Resources/Public/images/loading.gif";s:4:"9c92";s:41:"Resources/Public/images/missing_image.png";s:4:"279d";s:38:"Resources/Public/images/paintbrush.png";s:4:"247e";s:34:"Resources/Public/images/pencil.png";s:4:"a34e";s:31:"Resources/Public/images/raw.png";s:4:"dfcf";s:36:"Resources/Public/images/richtext.png";s:4:"97d0";s:36:"Resources/Public/images/tag_blue.png";s:4:"0824";s:37:"Resources/Public/images/tag_green.png";s:4:"8205";s:38:"Resources/Public/images/tag_orange.png";s:4:"97ce";s:36:"Resources/Public/images/tag_pink.png";s:4:"30f8";s:38:"Resources/Public/images/tag_purple.png";s:4:"af7c";s:35:"Resources/Public/images/tag_red.png";s:4:"5d63";s:38:"Resources/Public/images/tag_yellow.png";s:4:"c8a1";s:32:"Resources/Public/images/text.png";s:4:"2039";s:35:"Resources/Public/images/unknown.png";s:4:"b6ee";s:32:"Resources/Public/images/user.png";s:4:"3d3c";s:42:"Resources/Public/javascripts/formatJson.js";s:4:"23f4";s:47:"Resources/Public/javascripts/templatedisplay.js";s:4:"224b";s:48:"Resources/Public/stylesheets/templatedisplay.css";s:4:"bff5";s:14:"doc/manual.pdf";s:4:"74e3";s:14:"doc/manual.sxw";s:4:"440e";s:54:"interfaces/interface.tx_templatedisplay_customtype.php";s:4:"54b4";s:41:"samples/class.tx_templatedisplay_hook.php";s:4:"24a5";s:46:"samples/class.tx_templatedisplay_phonetype.php";s:4:"b146";s:16:"static/setup.txt";s:4:"ef08";}',
+	'suggests' => array(
+	),
 );
 
 ?>
