212d502b0ce9dc19228c6a453490c31101683d41
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-02-05 13:30:23 +0000

    Updated TYPO3 version compatibility
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/templatedisplay/trunk@57326 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '1.1.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.3.0-0.0.0',
+			'typo3' => '4.3.0-4.6.99',
 			'tesseract' => '0.2.0-0.0.0',
 		),
 		'conflicts' => array(
